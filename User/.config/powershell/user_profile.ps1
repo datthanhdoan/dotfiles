c363e4adc35818a9d4a058724f71cbb76b0d3733
DatThanhDoan <90030004+datthanhdoan@users.noreply.github.com>
2024-09-18 14:03:10 +0700

    Update user_profile.ps1

--- User/.config/powershell/user_profile.ps1
+++ User/.config/powershell/user_profile.ps1
@@ -1,11 +1,21 @@
 # alias 
-Set-Alias ll ls
 Set-Alias vim nvim
 Set-Alias vi nvim
 Set-Alias grep findstr
 Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
 Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
 Set-Alias cl clear
+
+# Replace ls with lsd
+Remove-Item alias:ls -Force
+function ls { lsd @args }
+Set-Alias -Name l -Value ls
+
+# Additional lsd aliases
+function la { lsd -a @args }
+function ll { lsd -l @args }
+function lla { lsd -la @args }
+function lt { lsd --tree @args }
 
 function psconfig {
     nvim $HOME\.config\powershell\user_profile.ps1
@@ -15,14 +25,15 @@ function psconfig {
 Set-PSReadLineOption -EditMode Emacs 
 Set-PSReadLineOption -BellStyle None
 Set-PSReadLineOption -PredictionSource History
-# lsd replace this 
- Import-Module -Name Terminal-Icons
 
+# Icons for lsd
+Import-Module -Name Terminal-Icons
 
 # Auto start ssh 
 #start-ssh-agent.cmd
+
 # theme
-oh-my-posh init pwsh --config $Home\2612.omp.json | Invoke-Expression
+oh-my-posh init pwsh --config $Home\tokyonight_storm.omp.json | Invoke-Expression
 
 # utilities
 function which ($command) {
